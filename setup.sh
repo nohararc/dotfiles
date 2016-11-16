8fc0b93660a80ed530c0c951f138b9696ae680d2
yusuke <yusuke@gmo>
2016-11-16 12:25:35 +0900

    modified .tmu.conf reload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #ln -s "/c/Users/yusuke-nohara/Documents/99_log/dotfiles/_vimrc" "/c/Users/yusuke-nohara/Desktop/vim80-kaoriya-win64"
 #ln -s "/c/Users/yusuke-nohara/Documents/99_log/dotfiles/_gvimrc" "/c/Users/yusuke-nohara/Desktop/vim80-kaoriya-win64"
-ln -s "/home/nohararc/dotfiles/.tmux.conf" "/home/nohararc/"
+ln -s "/home/yusuke-nohara/dotfiles/.tmux.conf" "/home/yusuke-nohara/"
